2d07996a9e0d458bbe02a0d190407ca3491dfe0f
Muskan Swarup <muskanswarup680@gmail.com>
2024-07-22 01:40:08 +0530

    create signup page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
   return (
     <div className='w-full flex justify-center items-center h-[70vh]'>
-      <div className='flex flex-col justify-center items-center space-y-4 w-[80%] md:w-[25%]'>
-        <h1 className='text-xl font-bold text-left text-[#2E765E]'>Login to Your Account</h1>
+      <div className='flex flex-col justify-center items-center space-y-6 w-[90%] md:w-[40%] lg:w-[33%] p-6 bg-white rounded-lg shadow-lg'>
+        <h1 className='text-2xl font-bold text-left text-[#2E765E]'>Login to Your Account</h1>
         <input type='text' className='w-full px-4 py-2 border-2 border-[#2E765E] outline-0 rounded-md' placeholder='Enter your email' />
-        <input type='password' className='w-full px-4 py-2 border-2 border-[#2E765E] outline-0 rounded-md' placeholder='Enter your password'/>
+        <input type='password' className='w-full px-4 py-2 border-2 border-[#2E765E] outline-0 rounded-md focus:border-teal-700' placeholder='Enter your password'/>
         <button className='w-full px-4 py-2 bg-[#2E765E] tracking-wide text-white text-lg font-bold rounded-lg hover:bg-teal-700'>Login</button>
+        <div className='flex justify-center items-center space-x-4 text-xs text-[#2E765E] font-semibold tracking-wide' >
+          <span>New here?</span>
+          <span className='hover:text-black'><Link to="/sign-up">Sign Up</Link></span>
+        </div>
       </div>
-      
     </div>
   )
 }
--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,10 +1,24 @@
-import React from 'react'
+import React from 'react';
+import { Link } from 'react-router-dom'
+
 
 const SignUp = () => {
   return (
-    <div>
-      SignUp
+    <div className='w-full flex justify-center items-center h-[70vh]'>
+    <div className='flex flex-col justify-center items-center space-y-6 w-[90%] md:w-[40%] lg:w-[33%] p-6 bg-white rounded-lg shadow-lg'>
+      <h1 className='text-2xl font-bold text-left text-[#2E765E]'>Create an Account</h1>
+      <input type='text' className='w-full px-4 py-2 border-2 border-[#2E765E] outline-0 rounded-md' placeholder='Enter your name' />
+      <input type='text' className='w-full px-4 py-2 border-2 border-[#2E765E] outline-0 rounded-md' placeholder='Enter your email' />
+      <input type='password' className='w-full px-4 py-2 border-2 border-[#2E765E] outline-0 rounded-md focus:border-teal-700' placeholder='Enter your password'/>
+      <button className='w-full px-4 py-2 bg-[#2E765E] tracking-wide text-white text-lg font-bold rounded-lg hover:bg-teal-700'>Login</button>
+      <div className='flex justify-center items-center space-x-4 text-xs text-[#2E765E] font-md tracking-wider' >
+        <p>Already have an account?</p>
+        <p className='hover:text-black'><Link to="/sign-up">Login</Link></p>
+      </div>
     </div>
+
+    
+  </div>
   )
 }
 
